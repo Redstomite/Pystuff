73227e3af988ef5b6ba2e55db077aa647c146fdb
Redstomite <rahul.r.prabhu@gmail.com>
2018-07-29 18:02:08 +0530

    Easier to read

--- wordch.py
+++ wordch.py
@@ -1,10 +1,10 @@
 dictionary = {"1":"e" , "2":"a" , "3":"d" , "4":"c"}
 Values_in_dictionary = dictionary.values()
 word = "abcx"
-word_converted_to_list = list(word)
-for value in word_converted_to_list:
+word_list = list(word)
+for value in word_list:
 	if value not in Values_in_dictionary:
 		print("Word letter not in dictionary")
 		break
 else:
-	print("Word letters are in dictionary")
+		print("Word letters are in dictionary")
